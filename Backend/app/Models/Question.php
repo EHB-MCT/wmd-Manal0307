2336b1346bf4c26f89cf22e66173960457ac3ad3
Manal Boulahya <manal.boulahya@student.ehb.be>
2025-12-02 15:24:28 +0100

    Question + QuestionAnswer models

---
+++ Backend/app/Models/Question.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order',
+        'question_text',
+        'question_key',
+    ];
+
+    public function answers(): HasMany
+    {
+        return $this->hasMany(QuestionAnswer::class);
+    }
+}
